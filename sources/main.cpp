1119f6d24ce57f3efb12c876403abfc6690fca7a
Jaime Chiroy <3002613570101@ingenieria.usac.edu.gt>
2021-08-03 00:29:42 -0600

    Main - Test - Lista Estudiantes

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "../headers/list_student.h"
 
 int main(){
+
+    ListStudent *students = new ListStudent();
+
+    students->showListContent();
+
+    students->insertStudent(201709020, "3002613570101", "Jaime Efrain Chiroy Chavez", "Ingeniería en Ciencias y Sistemas", "jaimefrain08@gmail.com", "asdf1234", 100, 25);
+    students->insertStudent(201700886, "3502451230508", "Emilia Miramontes", "Ingeniería en Ciencias y Sistemas", "nicols36@hotmail.com", "roxodiciqi", 205, 21);
+    students->insertStudent(201501786, "7249529279753", "Juan Curiel", "Ingeniería Industrial", "jernimo.caballero@yahoo.com", "zanolexima", 245, 24);
+
+    students->showListContent();
+
     return 0;
 }
