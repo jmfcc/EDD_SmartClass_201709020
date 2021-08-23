ea22b8dc4eaed84ba9f3ff8dbe33f2a382d226eb
jmfcc <3002613570101@ingenieria.usac.edu.gt>
2021-08-22 21:03:46 -0600

    Validaciones para datos de tareas

--- headers/validations.h
+++ headers/validations.h
@@ -11,9 +11,10 @@ bool validaLongitud(string value_, int longitud_);
 bool validaNumero(string value_);
 bool validaCorreo(string value_);
 //Tasks
-bool validaHora(int value_);
-bool validaDia(int value_);
-bool validaMes(int value_);
+bool validaHora(int value_); // 8 - 16 
+bool validaDia(int value_); // 1 - 30
+bool validaMes(int value_); // 7 - 11
+bool validaEstado(string value_);
 //Others
 bool cadenaVacia(string value_);
 
--- sources/validations.cpp
+++ sources/validations.cpp
@@ -11,7 +11,45 @@ bool validaNumero(string value_){
     }
     return true;
 }
-
 bool validaCorreo(string value_){
     return regex_match(value_, regex("[a-z0-9]+(\\.[a-z0-9]+)*@[a-z0-9]+(\\.[a-z0-9]+)*\\.(com|es|org)"));
+}
+bool cadenaVacia(string value_){
+    bool empty = true;
+    for (int i = 0; i < value_.length(); i++){
+        if (!isspace(value_[i])){
+            empty = false;
+            break;
+        }
+    }
+    return empty;
+}
+
+bool validaHora(int value_){
+    if (value_ >= 8 and value_ <=16){
+        return true;
+    }
+    return false;
+}
+bool validaDia(int value_){
+    if (value_ >= 1 and value_ <=30){
+        return true;
+    }
+    return false;
+}
+bool validaMes(int value_){
+    if (value_ >= 7 and value_ <=11){
+        return true;
+    }
+    return false;
+}
+
+bool validaEstado(string value_){
+    string typesStatus[4] = {"Pendiente", "Realizado", "Incumplido", "Cumplido"};
+    for (int i = 0; i < 4; i++){
+        if (value_ == typesStatus[i]){
+            return true;
+        }
+    }
+    return false;
 }
