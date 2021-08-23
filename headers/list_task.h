f9ba57f148a05710e2c994a2a8b377d980adfd87
jmfcc <3002613570101@ingenieria.usac.edu.gt>
2021-08-22 21:01:49 -0600

    Lista de tareas - Implementacion de matriz de datos

--- headers/list_task.h
+++ headers/list_task.h
@@ -2,30 +2,47 @@
 #define LISTTASK_H
 
 #include "node_task.h"
+#include "cola.h"
+#include "list_student.h"
+#include "validations.h"
 
 class ListTask{
 private:
     int size;
-    NodeTask *head;
+    ListStudent *refStud;
+    Cola *refError;
+    NodeTask *head; // List
+    NodeTask *myArrTask[5][30][9]; // Array
+    int generates;
 
 public:
     ListTask();
+
+    //Reference
+    void setColaRef(Cola *refError_);
+    void setStudentRef(ListStudent *refStud_);
 
     bool isEmpty();
     
     int getSize();
     void setSize(string type_);
 
-    void insertTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, string hour_, string status_);
+    void insertTaskArray(int indxM, int indxD, int indxH, int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_);
+
+    void insertTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_);
+    void insertErrorTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_, string infoErr_);
 
     void showListContent();
+    void showMatrixContent();
     
     void deleteTask(int id_);
 
     //Existence validations
     void searchTask(string cardNumber_, int id_);
-    
+    bool isTheDateAvaible(int indxM, int indxD, int indxH);
 
+    //Others
+    bool existCardNumber(int cardNumber_);
 };
 
 #endif
--- sources/list_task.cpp
+++ sources/list_task.cpp
@@ -3,6 +3,23 @@
 ListTask::ListTask(){
     this->size = 0;
     this->head = NULL;
+    for (int i = 0; i<5; i++){
+        for (int j = 0; j<30; j++){
+            for (int k = 0; k<9; k++){
+                this->myArrTask[i][j][k] = NULL;
+            }
+        }
+    }
+    this->generates = 1;
+    this->refError = NULL;
+    this->refStud = NULL;
+}
+
+void ListTask::setColaRef(Cola *refError_){
+    this->refError = refError_;
+}
+void ListTask::setStudentRef(ListStudent *refStud_){
+    this->refStud = refStud_;
 }
 
 bool ListTask::isEmpty(){
@@ -21,11 +38,16 @@ void ListTask::setSize(string type_){
     }
 }
 
-void ListTask::insertTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, string hour_, string status_){
+void ListTask::insertTaskArray(int indxM, int indxD, int indxH, int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_){
+    NodeTask *newNode = new NodeTask(0, cardNumber_, taskName_, taskDesc_, course_, date_, hour_, status_, month_, day_);
+    this->myArrTask[indxM][indxD][indxH] = newNode;
+}
+
+void ListTask::insertTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_){
 
     int id_ = this->size + 1;
     
-    NodeTask *newNode = new NodeTask(id_, cardNumber_, taskName_, taskDesc_, course_, date_, hour_, status_);
+    NodeTask *newNode = new NodeTask(id_, cardNumber_, taskName_, taskDesc_, course_, date_, hour_, status_, month_, day_);
     
     if (isEmpty()){
         //Asign newNode as head
@@ -40,6 +62,11 @@ void ListTask::insertTask(int cardNumber_, string taskName_, string taskDesc_, s
     }
     setSize("increase");
     // this->size++;
+}
+
+void ListTask::insertErrorTask(int cardNumber_, string taskName_, string taskDesc_, string course_, string date_, int hour_, string status_, int month_, int day_, string infoErr_){
+    NodeTask *newNode = new NodeTask(-1, cardNumber_, taskName_, taskDesc_, course_, date_, hour_, status_, month_, day_);
+    this->refError->queue(newNode, infoErr_);
 }
 
 void ListTask::showListContent(){
@@ -58,10 +85,42 @@ void ListTask::showListContent(){
     }
 }
 
+void ListTask::showMatrixContent(){
+    for (int m = 0; m < 5; m++){
+        for (int d = 0; d < 30; d++){
+            string concat = "";
+            if (d < 10){
+                concat += "D#"+ to_string(d) + " : ";
+            } else {
+                concat += "D#"+ to_string(d) + ": ";
+            }
+            for (int h = 0; h < 9; h++){
+                if (this->myArrTask[m][d][h] == NULL){
+                    concat += "-";
+                } else {
+                    concat += "8";
+                }
+            }
+            cout<<concat<<endl;
+        }
+        cout<<endl;
+    }
+}
+
 void ListTask::deleteTask(int id_){
     //Not implemented
 }
 
 void ListTask::searchTask(string cardNumber_, int id_){
     //Not implemented
+}
+
+bool ListTask::isTheDateAvaible(int indxM, int indxD, int indxH){
+    if (this->myArrTask[indxM][indxD][indxH] == NULL){
+        return true;
+    }
+    return false;   
+}
+bool ListTask::existCardNumber(int cardNumber_){
+    return refStud->searchStudentByCardNumber(cardNumber_);
 }
