2124faf7531d5117c7e0ff67f8850caaedf50178
jmfcc <3002613570101@ingenieria.usac.edu.gt>
2021-09-06 04:58:14 -0600

    Lista de semestres - Funcion insertar, buscar / sin cursos

---
+++ web_app/server/List_Semester.py
@@ -0,0 +1,45 @@
+class NodeSemester:
+
+    def __init__(self, semester):
+        self.semester = semester
+        #SpecialPointer
+        self.courses = None  # Not implemented
+        #Pointer
+        self.next = None
+    
+class ListSemester:
+
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def isEmpty(self):
+        return self.head == None
+    
+    def insertSemester(self, sem):
+        newNode = NodeSemester(sem)
+        if self.isEmpty():
+            self.head = newNode
+        else:
+            if sem < self.head.semester:
+                newNode.next = self.head
+                self.head = newNode
+            else:
+                self.head.next = newNode
+        self.size += 1
+    
+    def searchSemester(self, sem):
+        aux = self.head
+        while aux is not None:
+            if sem == aux.semester:
+                return True
+            aux = aux.next
+        return False
+    
+    def getSemester(self, sem):
+        aux = self.head
+        while aux is not None:
+            if sem == aux.semester:
+                return aux
+            aux = aux.next
+        return None
