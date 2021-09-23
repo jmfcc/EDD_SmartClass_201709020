a72855cb7ed64744da71f699db854ae764e37fe9
jmfcc <3002613570101@ingenieria.usac.edu.gt>
2021-09-23 01:09:15 -0600

    Endpoints Eliminar, Actualizar de Tareas - Reporte de Matriz Dispersa de Tareas

--- web_app/server/App_SmartClass.py
+++ web_app/server/App_SmartClass.py
@@ -3,7 +3,7 @@ from os import name
 from flask import Flask, json, jsonify, request, Response
 from flask_cors import CORS
 from Student_AVL import StudentAVL
-from Graph_Functions import graphDoubleList, graphTreeAVL
+from Graph_Functions import graphDoubleList, graphTreeAVL, graphDMatrix
 
 records = StudentAVL()
 
@@ -40,7 +40,7 @@ def splitHour(hour_):
 def helloWorld():
   return "Hello, cross-origin-world!"
 
-@app.route("/carga", methods=["POST"])
+@app.route("/carga", methods=["POST"]) ################################################
 def loadFile():
   try:
     data = request.get_json(force=True)
@@ -53,19 +53,40 @@ def loadFile():
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
 
-@app.route("/reporte", methods=["GET"])
+@app.route("/reporte", methods=["GET"]) ###############################################
 def report():
   try:
     data = request.get_json(force=True)
     type_ = data["tipo"]
     #Validation of type -----------------------------------------------
     msg = "Try generate a report"
-    if type_ == 0:
+    if type_ == 0: # AVL REPORT
       graphTreeAVL(records)
-    elif type_ == 1:
+      msg = " >> Info: Arbol AVL de estudiantes generado exitosamente"
+    elif type_ == 1: # DISP MATRIX TASK
+      cardnumber_ = data["carnet"]
+      year_ = data["año"]
+      month_ = data["mes"]
       #----------------------------------------------------------------
-      pass
-    elif type_ == 2:
+      if isValid(cardnumber_, year_, month_):
+        if records.searchStudent(cardnumber_):
+          data = records.getStudent(cardnumber_) # NodeStudent
+          if not data.years.isEmpty() and data.years.searchYear(int(year_)):
+            data = data.years.getYear(int(year_)) # NodeYear
+            if not data.months.isEmpty() and data.months.searchMonth(month_):
+              data = data.months.getMonth(month_) # NodeMonth
+              # data = data.calendar.getCellCalendar(hour_, day_) # NodeCellTask
+              graphDMatrix(data.calendar)
+              msg = " >> Info: Grafo matriz de tareas generado exitosamente"
+            else:
+              msg = " >> Error: no hay registros del mes {}".format(str(year_))
+          else:
+            msg = " >> Error: no hay registros del año {}".format(str(year_))
+        else:
+          msg = " >> Error: No hay registros de {}".format(str(cardnumber_))
+      else:
+        msg = " >> Error: Verifique sus datos"
+    elif type_ == 2: # LINKEDLIST TASK
       cardNumber_ = data["carnet"]
       year_ = data["año"]
       month_ = data["mes"]
@@ -82,6 +103,7 @@ def report():
               if data is not None:
                 dataStud = 'Carne: {}\\nFecha: {}/{}/{}\\nHora: {}:00'.format(cardNumber_, str(day_), str(month_), str(year_), str(hour_))
                 graphDoubleList(data.accesListTasks, dataStud)
+                msg = " >> Info: Lista de tareas generado exitosamente"
               else:
                 msg = " >> Error: no hay registros en la fecha {}/{}/{} y hora {}:00".format(str(day_), str(month_), str(year_), str(hour_))
             else:
@@ -90,8 +112,12 @@ def report():
             msg = " >> Error: no hay registros del año {}".format(str(year_))
         else:
           msg = " >> Error: No hay registros de {}".format(str(cardNumber_))
-    #     ----- NOT IMPLEMENTED -----------
-    #     ----- NOT IMPLEMENTED -----------
+      else:
+        msg = " >> Error: Verifique sus datos"
+    elif type_ == 3: # B-TREE PENSUM
+      pass
+    elif type_ == 4: # B-TREE COURSES
+      pass
     return jsonify({ "response" : msg })
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
@@ -112,8 +138,7 @@ def studentInsert():
     credits_ = data["creditos"]
     age_ = data["edad"]
     msg = "I'm saving a student data " 
-    #Validation of type -----------------------------------------------    
-    # cardnumber_ = (cardnumber_)
+    #Validation of type -----------------------------------------------
     if isValid(cardnumber_, dpi_, name_, carrer_, email_, passw_, credits_, age_):
       if records.searchStudent(cardnumber_):
         msg = " >> Info: El carnet {} ya está registrado".format(cardnumber_)
@@ -122,8 +147,6 @@ def studentInsert():
         msg = " >> Info: Datos del estudiante almacenados correctamente" 
     else:
       msg = " >> Error: Verifique sus datos"
-    # ----------------------------------------------------------------------------------------
-    
     return jsonify({ "response" : msg})
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
@@ -157,19 +180,16 @@ def studentUpdate():
         msg = " >> Error: No hay registros del carnet {}".format(cardnumber_)
     else:
       msg = " >> Error: Verifique sus datos"
-    #     ----- NOT IMPLEMENTED -----------
-    #     ----- NOT IMPLEMENTED -----------
     return jsonify({ "response" : msg})
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
 
-@app.route("/estudiante", methods=["DELETE"])  ######################################
+@app.route("/estudiante", methods=["DELETE"])  ###########################################
 def studentDelete():
   try:
     data = request.get_json(force=True)
     cardnumber_ = data["carnet"]
     #Validation of type -----------------------------------------------
-    #     ----- NOT IMPLEMENTED -----------
     #     ----- NOT IMPLEMENTED -----------
     return jsonify({ "response" : "I'm deleting a student with cardnumber:" + cardnumber_ })
   except:
@@ -261,24 +281,90 @@ def taskUpdate():
     date_ = data["fecha"]
     hour_ = data["hora"]
     status_ = data["estado"]
+    pos_ = data["posicion"]
+    msg = "Actualzando una tarea"
     #Validation of type -----------------------------------------------
+    if isValid(cardnumber_, name_, desc_, course_, date_, hour_, status_):
+      if records.searchStudent(cardnumber_):
+        data = records.getStudent(cardnumber_) # NodeStudent
+        valid, day_, month_, year_ = splitDate(date_)
+        if valid:
+          if not data.years.isEmpty() and data.years.searchYear(year_):
+            data = data.years.getYear(year_) # NodeYear
+            if not data.months.isEmpty() and data.months.searchMonth(month_):
+              hour_ = splitHour(hour_)
+              data = data.months.getMonth(month_) # NodeMonth
+              data = data.calendar.getCellCalendar(hour_, day_) # NodeCellTask
+              if data is not None:
+                taskData = data.accesListTasks.getTask(pos_) # NodeTask
+                taskData.name = name_
+                taskData.description = desc_
+                taskData.course = course_
+                taskData.status = status_
+                msg = " >> Info: Se han actualizado los datos de la tarea"
+              else:
+                msg = " >> Error: no hay registros en la fecha {}/{}/{} y hora {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+            else:
+              msg = " >> Error: No hay tarea registradas para {}/{}/{}  {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+          else:
+            msg = " >> Error: No hay tareas registradas para {}/{}/{}  {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+        else:
+          msg = " >> Error: La fecha {} no tiene el formato correcto".format(date_)
+      else:  
+        msg = " >> Error: El carnet no existe"
+    else:
+      msg = " >> Error: Verifique sus datos"
     #     ----- NOT IMPLEMENTED -----------
     #     ----- NOT IMPLEMENTED -----------
-    return jsonify({ "response" : "I'm will update a task for student with cardnumber:" + cardnumber_ })
+    return jsonify({ "response" : msg})
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
 
-@app.route("/recordatorios", methods=["GET"]) #############################################
+@app.route("/recordatorios", methods=["GET"])
 def taskSelect():
   try:
     data = request.get_json(force=True)
     cardnumber_ = data["carnet"]
     date_ = data["fecha"]
     hour_ = data["hora"]
+    pos_ = data["posicion"]
+    msg = "Obteniendo una tarea"
     #Validation of type -----------------------------------------------
-    #     ----- NOT IMPLEMENTED -----------
-    #     ----- NOT IMPLEMENTED -----------
-    return jsonify({ "response" : "I'm will select a task for student with cardnumber:" + cardnumber_ })
+    if isValid(cardnumber_, date_, hour_, pos_):
+      if records.searchStudent(cardnumber_):
+        data = records.getStudent(cardnumber_) # NodeStudent
+        valid, day_, month_, year_ = splitDate(date_)
+        if valid:
+          if not data.years.isEmpty() and data.years.searchYear(year_):
+            data = data.years.getYear(year_) # NodeYear
+            if not data.months.isEmpty() and data.months.searchMonth(month_):
+              hour_ = splitHour(hour_)
+              data = data.months.getMonth(month_) # NodeMonth
+              data = data.calendar.getCellCalendar(hour_, day_) # NodeCellTask
+              if data is not None:
+                taskData = data.accesListTasks.getTask(pos_) # NodeTask
+                msg = {
+                  "Carnet":cardnumber_,
+                  "Fecha":date_,
+                  "Hora": str(hour_)+":00",
+                  "Nombre":taskData.name,
+                  "Descripcion":taskData.description,
+                  "Curso":taskData.course,
+                  "Estado":taskData.status
+                }
+              else:
+                msg = " >> Error: no hay registros en la fecha {}/{}/{} y hora {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+            else:
+              msg = " >> Error: No hay tarea registradas para {}/{}/{}  {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+          else:
+            msg = " >> Error: No hay tareas registradas para {}/{}/{}  {}:00".format(str(day_), str(month_), str(year_), str(hour_))
+        else:
+          msg = " >> Error: La fecha {} no tiene el formato correcto".format(date_)
+      else:  
+        msg = " >> Error: El carnet no existe"
+    else:
+      msg = " >> Error: Verifique sus datos"
+    return jsonify({ "response" : msg})
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
 
@@ -313,17 +399,15 @@ def taskDelete():
         msg = " >> Error: El carnet no existe"
     else:
       msg = " >> Error: Verifique sus datos"
-        
-    #     ----- NOT IMPLEMENTED -----------
-    #     ----- NOT IMPLEMENTED -----------
+
     return jsonify({ "response" : msg})
   except:
     return jsonify({ "response" : "Something goes wrong, verify your data"})
 
 # ---------------------------------------------------------------------------------------------------------------------
-# ---                                          CRUD CURSOS                                                          ---    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# ---                                          CRUD CURSOS                                                          ---
 # ---------------------------------------------------------------------------------------------------------------------
-@app.route("/cursosEstudiante", methods=["POST"])
+@app.route("/cursosEstudiante", methods=["POST"])  ########################################
 def courseInsert():
   try:
     data = request.get_json(force=True)
